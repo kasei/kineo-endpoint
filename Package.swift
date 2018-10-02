c6a8141f810bf8e9b3146ff951bf32fe780a2594
Gregory Todd Williams <greg@evilfunhouse.com>
2018-10-02 08:48:52 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -4,10 +4,14 @@ import PackageDescription
 let package = Package(
     name: "kineo-endpoint",
     dependencies: [
-        .package(url: "https://github.com/kasei/kineo.git", from: "0.0.12"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.20")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.0.0")),
     ],
     targets: [
+    	.target(
+    		name: "kineo-create-db",
+            dependencies: ["Kineo"]
+    	),
         .target(
             name: "kineo-endpoint",
             dependencies: ["Kineo", "Vapor"]
