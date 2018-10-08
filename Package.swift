160a559f05a46de54383f1a54a40ebf2ba47cf4f
Gregory Todd Williams <greg@evilfunhouse.com>
2018-10-08 07:49:44 -0700

    Refactored endpoint code into a modular KineoEndpoint library.

--- Package.swift
+++ Package.swift
@@ -3,18 +3,25 @@ import PackageDescription
 
 let package = Package(
     name: "kineo-endpoint",
+	products: [
+		.library(name: "KineoEndpoint", targets: ["KineoEndpoint"]),
+	],    
     dependencies: [
         .package(url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.21")),
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.0.0")),
     ],
     targets: [
     	.target(
+    		name: "KineoEndpoint",
+			dependencies: ["Kineo", "Vapor"]
+    	),
+    	.target(
     		name: "kineo-create-db",
             dependencies: ["Kineo"]
     	),
         .target(
             name: "kineo-endpoint",
-            dependencies: ["Kineo", "Vapor"]
+            dependencies: ["KineoEndpoint", "Vapor"]
         ),
     ]
 )
