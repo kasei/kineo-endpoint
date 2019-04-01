984631335fc4d07db5c45168c4c47ee5fdfe1b02
Gregory Todd Williams <greg@evilfunhouse.com>
2019-04-01 08:48:10 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 		.library(name: "KineoEndpoint", targets: ["KineoEndpoint"]),
 	],    
     dependencies: [
-        .package(url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.67")),
+        .package(url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.68")),
         .package(url: "https://github.com/kasei/swift-hdt.git", .upToNextMinor(from: "0.0.5")),
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.2.0")),
         .package(url: "https://github.com/alexaubry/HTMLString", .upToNextMinor(from: "4.0.0")),
