4e3870d32aa613d90bf87f8e49c664e7705031f1
Gregory Todd Williams <greg@evilfunhouse.com>
2020-06-13 09:51:08 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         	targets: ["kineo-endpoint"]),
 	],    
     dependencies: [
-       .package(name: "Kineo", url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.83")),
-//        .package(name: "Kineo", url: "https://github.com/kasei/kineo.git", .branch("sparql-12")),
+//       .package(name: "Kineo", url: "https://github.com/kasei/kineo.git", .upToNextMinor(from: "0.0.83")),
+       .package(name: "Kineo", url: "https://github.com/kasei/kineo.git", .branch("sparql-12")),
         .package(name: "Vapor", url: "https://github.com/vapor/vapor.git", from: "3.2.0"),
         .package(name: "HTMLString", url: "https://github.com/alexaubry/HTMLString", .upToNextMinor(from: "4.0.0")),
        .package(name: "Diomede", url: "https://github.com/kasei/diomede.git", .upToNextMinor(from: "0.0.29")),
